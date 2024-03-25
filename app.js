3c3d896cd97a0e73eb156d0201f3bbfba3b1ff05
Your Name <kty8600@naver.com>
2024-03-25 16:33:05 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,35 @@
+
+//모듈
+var express = require('express')
+var cors = require('cors')
+var app = express()
+const PORT = 3000;
+ 
+app.use(cors())
+
+//라우팅
+const home = require("./routes/home");
+
+
+//앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들웨어
+
+module.exports = app;
+
+// app.get('/sound/:name', function (req, res) {
+//     const { name } = req.params
+
+//     if(name == "dog"){
+//         res.json({'sound' : '멍멍'})
+//     } else if (name == 'cat') {
+//         res.json({'sound' : '냐옹'})        
+//     } else {
+//         res.json({'sound' : '알수없음'})
+//     }
+//     console.log(name)
+
+// })
+

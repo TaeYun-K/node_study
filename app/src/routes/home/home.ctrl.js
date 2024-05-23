befa6df78bd84382f7996ba66187140058235ff6
Your Name <kty8600@naver.com>
2024-05-23 17:47:37 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const User = require("../../models/User");
 const UserStorage = require("../../models/UserStorage")
 
 
@@ -18,24 +19,28 @@ const output = {
 
 const process = {
     login : (req,res) => {
-        const id = req.body.id,
-            psword = req.body.psword
+        const user = new User(req.body)
+        const response = user.login();
+        return res.json(response);
 
-        const users = UserStorage.getUsers("id", "psword")
+        // const id = req.body.id,
+        //     psword = req.body.psword
 
-        const response = {};
+        // const users = UserStorage.getUsers("id", "psword")
 
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id)
-            if (users.psword[idx] === psword) {
-                response.success = true
-                return res.json(response)
-            }
-        }
+        // const response = {};
 
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다."
-        return res.json(response)
+        // if (users.id.includes(id)) {
+        //     const idx = users.id.indexOf(id)
+        //     if (users.psword[idx] === psword) {
+        //         response.success = true
+        //         return res.json(response)
+        //     }
+        // }
+
+        // response.success = false;
+        // response.msg = "로그인에 실패하셨습니다."
+        // return res.json(response)
 
     }
 

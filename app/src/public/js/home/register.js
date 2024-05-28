774190384c1109faf1aee611ef752e33e198f426
Your Name <kty8600@naver.com>
2024-05-28 14:54:16 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,19 +1,23 @@
 'use strict';
 
 const id = document.querySelector("#id"),
+name = document.querySelector("#name"),
 psword = document.querySelector("#psword"),
-loginBtn = document.querySelector('#button');
+confirmPsword = document.querySelector("#confirm-psword"),
+registerBtn = document.querySelector('#button');
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login() {
+function register() {
     const req = {
         id: id.value,
-        psword: psword.value
+        name : name.value,
+        psword: psword.value,
+        confirmPsword : confirmPsword.value
     }
      console.log(req)
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type" : "application/json",
@@ -23,7 +27,7 @@ function login() {
     .then((res) => res.json())
     .then((res) => {
         if (res.success) {
-            location.href = "/";
+            location.href = "/login";
         } else {
             alert(res.msg);
         }
